4fb997861a564eda55c11911ea5b0ceca45f4195
rperiago <periago.ricardo@gmail.com>
2019-09-07 17:03:39 -0300

    Updates

---
+++ precache-manifest.f41dd842156c77bfb965d5a343de354d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "80174758ce7da7a9bbce214ede7a97dd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3ae4d9d333476291b0f0",
+    "url": "/static/css/main.cb0571cd.chunk.css"
+  },
+  {
+    "revision": "5e09e8179f6d8756957d",
+    "url": "/static/js/2.14afdf4a.chunk.js"
+  },
+  {
+    "revision": "3ae4d9d333476291b0f0",
+    "url": "/static/js/main.931c003c.chunk.js"
+  },
+  {
+    "revision": "97ef201e630492c563b2",
+    "url": "/static/js/runtime~main.13057f11.js"
+  },
+  {
+    "revision": "519fe5a2fb1f4eac410c1b6bcebb1c49",
+    "url": "/static/media/camilla.519fe5a2.jpeg"
+  }
+]);
