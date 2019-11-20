da47c126b99dbccb1ecea7e5c9c043feca3e7b9c
Periago <periago.ricardo@gmail.com>
2019-11-20 00:29:22 -0300

    build

---
+++ build/precache-manifest.d3ba9d20a0d66fae03e91b9be2ddeeed.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "047b7e0c9d39cfae5bd2ffde78df9b0b",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "032cf4442fa4c72f9565",
+    "url": "/portfolio/static/css/main.9f7801e4.chunk.css"
+  },
+  {
+    "revision": "49acdf1989e6ffd921f7",
+    "url": "/portfolio/static/js/2.2c2ef11d.chunk.js"
+  },
+  {
+    "revision": "032cf4442fa4c72f9565",
+    "url": "/portfolio/static/js/main.a02189b1.chunk.js"
+  },
+  {
+    "revision": "a2b8957add1ac21263cd",
+    "url": "/portfolio/static/js/runtime~main.2af0308f.js"
+  },
+  {
+    "revision": "dac53250dc8fb4d960a1663b24f30cf1",
+    "url": "/portfolio/static/media/321comm.dac53250.png"
+  },
+  {
+    "revision": "ee89c559ba63cba1f3d27b5444d1b051",
+    "url": "/portfolio/static/media/bs-mobile-2.ee89c559.png"
+  },
+  {
+    "revision": "662fef514296322c04dc4c3a173e8eb2",
+    "url": "/portfolio/static/media/bs-mobile.662fef51.png"
+  },
+  {
+    "revision": "519fe5a2fb1f4eac410c1b6bcebb1c49",
+    "url": "/portfolio/static/media/camilla.519fe5a2.jpeg"
+  },
+  {
+    "revision": "4bed2c692fef536104d0ab15cbab26a3",
+    "url": "/portfolio/static/media/camillacamilla.4bed2c69.png"
+  },
+  {
+    "revision": "2e8f6c8d23b3c0237b96b6dfc518effa",
+    "url": "/portfolio/static/media/decision6.2e8f6c8d.png"
+  },
+  {
+    "revision": "58f4ef1aedd89bc29e3ae886f05bc8f2",
+    "url": "/portfolio/static/media/heatmap.58f4ef1a.png"
+  },
+  {
+    "revision": "af250cb3198a3763191e499d92e5f0e7",
+    "url": "/portfolio/static/media/nicephotos.af250cb3.png"
+  },
+  {
+    "revision": "ba0eb3f7e8e027232f10452797eafac6",
+    "url": "/portfolio/static/media/photoshop.ba0eb3f7.svg"
+  },
+  {
+    "revision": "55d930f4647ef7718b114862797ab855",
+    "url": "/portfolio/static/media/usertest.55d930f4.png"
+  },
+  {
+    "revision": "8881025c292daae4a3c66eab0feb357b",
+    "url": "/portfolio/static/media/workshop.8881025c.png"
+  }
+]);
